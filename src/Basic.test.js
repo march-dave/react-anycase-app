37c8891ebfe81479418268686b8f59383535cbee
march-dave <ysangy@gmail.com>
2019-02-08 19:47:27 -0500

    add Basic test

--- src/Basic.test.js
+++ src/Basic.test.js
@@ -1,16 +1,17 @@
 import React from 'react';
-// import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer';
+import Basic from './Basic'
 // import Counter from './Counter';
 
-// describe('Counter', () => {
-//   let component = null;
+describe('Counter', () => {
+  let component = null;
 
-//   it('renders correctly', () => {
-//     component = renderer.create(<Counter />);
-//   });
+  it('renders correctly', () => {
+    component = renderer.create(<Basic />);
+  });
 
 //   it('matches snapshot', () => {
 //     const tree = component.toJSON();
 //     expect(tree).toMatchSnapshot();
 //   })
-// }); 
+}); 
