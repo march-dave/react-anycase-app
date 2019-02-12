3cefe5f5398a179d317993dd69b4e5ed66171294
march-dave <ysangy@gmail.com>
2019-02-12 10:38:39 -0500

    enzyme unit test

--- src/Basic.test.js
+++ src/Basic.test.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+// import renderer from 'react-test-renderer';
 import Basic from './Basic'
-// import Counter from './Counter';
+import Adapter from "enzyme-adapter-react-16";
+import { shallow, configure } from "enzyme";
+
+configure({ adapter: new Adapter() });
+
 
 describe('Counter', () => {
   let component = null;
 
-  it('renders correctly', () => {
-    component = renderer.create(<Basic />);
+  it("renders correctly", () => {
+    component = shallow(<Basic />);
   });
 
-  it('matches snapshot', () => {
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  })
+  it("matches snapshot", () => {
+    expect(component).toMatchSnapshot();
+  });
+
+
+  // it('renders correctly', () => {
+  //   component = renderer.create(<Basic />);
+  // });
+
+  // it('matches snapshot', () => {
+  //   const tree = component.toJSON();
+  //   expect(tree).toMatchSnapshot();
+  // })
 }); 
