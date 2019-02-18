c3b861c80c2f6d48c8bd964f4b6862e83f1144a1
march-dave <ysangy@gmail.com>
2019-02-18 15:43:50 -0500

    enzyme script update

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,18 +1,4 @@
-// import { configure } from "enzyme";
-// import Enzyme from "enzyme";
-// import { Adapter } from "enzyme-adapter-react-16";
-
-// Enzyme.configure({ adapter: new Adapter() });
-
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
-
-// import Enzyme from "enzyme";
-// import EnzymeAdapter from "enzyme-adapter-react-16";
-
-// Enzyme.configure({
-//     adapter: new EnzymeAdapter(),
-//     disableLifecycleMethods: true
-// })
