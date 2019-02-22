fabe2a6b6c359c40a700c8ea5560c9e4061f383d
Dave Lee <ysangy@gmail.com>
2019-02-22 12:33:45 -0500

    enzyme function to use global

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,13 @@
+import React from "react";
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
+import { shallow, mount, render } from "enzyme";
+// import sinon from "sinon";
+
 Enzyme.configure({ adapter: new Adapter() });
+
+global.React = React;
+global.shallow = shallow;
+global.mount = mount;
+// global.sinon = sinon;
