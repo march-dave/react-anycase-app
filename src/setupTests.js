f3acaf1c996b15e8c5a8b950f20b4a8c6515ff4e
Dave Lee <ysangy@gmail.com>
2019-02-22 13:01:29 -0500

    add enzyme serialization config in script which is optional setup in package.json or in the script

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,11 +3,14 @@ import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 import { shallow, mount, render } from "enzyme";
+import { createSerializer } from "enzyme-to-json";
 // import sinon from "sinon";
+expect.addSnapshotSerializer(createSerializer({ mode: "deep" }));
 
 Enzyme.configure({ adapter: new Adapter() });
 
 global.React = React;
 global.shallow = shallow;
+global.render = render;
 global.mount = mount;
 // global.sinon = sinon;
--- src/test/Basic.test.js
+++ src/test/Basic.test.js
@@ -1,6 +1,14 @@
 import React from "react";
 import Basic from "../Basic";
-import { shallow } from "enzyme";
+// import { shallow } from "enzyme";
+
+
+import configureMockStore from "redux-mock-store";
+import configureStore from "../configureStore";
+
+import { Provider } from "react-redux";
+import action from "../Action";
+import reducer from "../Reducer";
 
 const setUp = (props = {}) => {
   const component = shallow(<Basic {...props} />);
@@ -11,23 +19,21 @@ describe("Counter", () => {
   let component = null;
 
   beforeEach(() => {
-    
     let wrapper;
     const props = {
       header: "Test Header",
       desc: "Test Desc"
     };
     wrapper = setUp(props);
-    
+
     component = setUp();
   });
 
   it("renders correctly", () => {
-    component = shallow(<Basic />);
+    component = setUp();
   });
 
   it("matches snapshot", () => {
     expect(component).toMatchSnapshot();
   });
-
 });
