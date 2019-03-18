d72297e6522a7c273ee3964946b0bd19c37e95c6
Administrator <ysangy@gmail.com>
2019-03-17 20:30:44 -0400

    css practice

--- src/TimeLine.js
+++ src/TimeLine.js
@@ -14,7 +14,7 @@ class TimeLine extends Component {
           <ul className="timeline">
             <li className="timeline-item">
               <div className="card">
-                <img className="card-icon" src="images/1.png" />
+                <img className="card-icon" src="images/1.png"  alt="images" />
                 <h2 className="card-title">1990</h2>
                 <dl className="card-definition">
                   <dt>
