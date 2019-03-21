76fa500640093850736568e01046daa9db5ff45c
Administrator <ysangy@gmail.com>
2019-03-20 21:37:36 -0400

    ES6 for in for of practice

---
+++ src/ES6ForInOf.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+
+class ES6ForInOf extends Component {
+    render() {
+        return(
+            <div>
+                {
+                    // function anagrams(stringA, stringB) {
+                    //   const aCharMap = buildCharMap(stringA);
+                    //   const bCharMap = buildCharMap(stringB);
+                    //
+                    //   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+                    //     return false;
+                    //   }
+                    //
+                    //   for (let char in aCharMap) {
+                    //     if (aCharMap[char] !== bCharMap[char]) {
+                    //       return false;
+                    //     }
+                    //   }
+                    //
+                    //   return true;
+                    // }
+                    //
+                    // function buildCharMap(str) {
+                    //   const charMap = {};
+                    //
+                    //   for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+                    //     charMap[char] = charMap[char] + 1 || 1;
+                    //   }
+                    //
+                    //   return charMap;
+                    // }
+                }
+            </div>
+        );
+    }
+}
+
+export default ES6ForInOf;
