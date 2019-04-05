7f50854f142272da9cb21df1b8cf0f26979c3d4a
Administrator <ysangy@gmail.com>
2019-04-04 22:03:02 -0400

    up to date

--- src/Counter.js
+++ src/Counter.js
@@ -2,11 +2,6 @@ import React, { Component } from "react";
 import debounce from "lodash.debounce";
 
 class Counter extends Component {
-  //   constructor(props) {
-  //     super(props);
-  //     this.handlerSearchDelayed = debounce(this.onSearch, 1000);
-  //     this.handlerSearchDelayed = this.handlerSearchDelayed.bind(this);
-  //   }
 
   state = {
     number: 0
@@ -16,23 +11,7 @@ class Counter extends Component {
     debounce(this.onSearch, 1000);
   };
 
-  // onSearch(textSearch) {
-  //     this.setState({text:textSearch});
-  //     this.getData();
-  // }
-
-  // handleChange = () => {
-  //     this.setState({
-  //         number: this.state.number = this.state.number + 1
-  //     })
-  // }
-
-  // handleChange = () => {
-  //     this.setState(({number}) => ({ number: number + 1 }));
-  // }
-
   handleChange = () => {
-    //    this.setState( ({number}) => ({number: number + 1}) )
 
     this.setState(({ number }) => ({ number: number + 1 }));
   };
