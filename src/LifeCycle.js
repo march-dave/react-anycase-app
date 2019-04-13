633164e4c62b3fa44e04dec87ce8f3d9576f09bf
Administrator <ysangy@gmail.com>
2019-04-13 10:26:07 -0400

    up to date

--- src/LifeCycle.js
+++ src/LifeCycle.js
@@ -15,6 +15,8 @@ export default class LifeCycle extends Component {
       }
     
       componentDidMount() {
+        // 외부 라이브러리 연동 Ajax 콜
+        // DOM의 속성을 읽거나 변경하는 작업도 여기
         console.log('componentDidMount');
       }
     
@@ -30,9 +32,16 @@ export default class LifeCycle extends Component {
       }
       
       componentDidUpdate(prevProps, prevState) {
+        // 컴포넌트에서 render 호출 후 다음에 발생
+        // this.props 와  this.state 가 바뀐다.
+        // 이전 값인 prevProps, prevState 를 조회 할 수 있다.
         console.log('componentDidUpdate');
       }
       
+      componentWillUnmount() {
+        // 컴포넌트가 더 이상 필요 하지 않게 되면 호출 된다.
+        // 이벤트 setTimer 외부 라이브러리 인스턴스
+      }
     
       handleIncrease = () => {
         const { number } = this.state;
