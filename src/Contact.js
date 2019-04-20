a044cc9e76ec470780d79e6d88510ff4c13c80d5
Administrator <ysangy@gmail.com>
2019-04-19 20:06:50 -0400

    up to date

--- src/Contact.js
+++ src/Contact.js
@@ -2,16 +2,31 @@ import React, { Component } from 'react';
 
 class Contact extends Component {
 
-
+    id = 2;
     state = {
+        information: [
+            {
+                id: 0,
+                firstname: 'dave'
+            },
+            {
+                id: 1,
+                firstname: 'jane'
+            }
+        ]
+    }
 
-        
+    handClick = (e) => {
+        this.setState({
+            id: ++this.id,
+            [e.target.name]: e.target.value
+        })
     }
 
     render() {
         return (
             <div>
-
+                <input type="text" name="firstname" onClick={this.handClick} />
             </div>
         )
     }
