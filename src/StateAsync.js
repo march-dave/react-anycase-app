1bdd2ead992b83a1408fc0a1aa4df88fa3e18f46
Administrator <ysangy@gmail.com>
2019-04-22 18:52:02 -0400

    state call back

---
+++ src/StateAsync.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class StateAsync extends Component {
+
+
+
+// this.setState({
+//     name:'value' 
+// },() => {
+//     console.log(this.state.name);
+// });
+
+
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
