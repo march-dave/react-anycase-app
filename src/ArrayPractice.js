326b4c0493704faf4d0545bb956fbe39a59f683b
Administrator <ysangy@gmail.com>
2019-04-25 18:14:12 -0400

    up to date

---
+++ src/ArrayPractice.js
@@ -0,0 +1,10 @@
+// const exampleArray = [];
+//     let iTemp = 0;
+//     props.listOfQuestions.map((item, index) => {
+//       _.range(props.listOfQuestions[index].numberOfFields).map((c, i) => {
+//         console.log(iTemp++)
+
+//         console.log(props.listOfQuestions[index].example)
+//         exampleArray.push(props.listOfQuestions[index].example);
+//       });
+//     })
