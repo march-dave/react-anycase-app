dd1943885a4703fac535fb5f911b56fd498d5a19
Administrator <ysangy@gmail.com>
2019-04-29 20:22:59 -0400

    up to date

--- src/StateAsync.js
+++ src/StateAsync.js
@@ -10,6 +10,15 @@ export default class StateAsync extends Component {
 //     console.log(this.state.name);
 // });
 
+// this.setState(prevState => ({
+//   currentPage: prevState.currentPage + 1,
+//   currentExercise: res.updatedResponse,
+//   fullSentence: decodeString(res.updatedResponse.exercise_responses.fullSentence),
+//   edit: true,
+// }));
+
+// input::placeholder {
+
 
 
   render() {
