a5328fc60ce1603f8327816fdf8d73f5811bd1c8
march-dave <ysangy@gmail.com>
2019-05-17 22:35:51 -0400

    es6 export module

---
+++ src/ES6Module.js
@@ -0,0 +1,21 @@
+// export const sqrt = Math.sqrt;
+// export function square(x) {
+//     return x * x;
+// }
+
+// export function diag(x, y) {
+//     return sqrt(square(x) + square(y));
+// }
+
+// import {square} from  'lib';
+// console.log(square(11));
+// console.log(square(4, 3));
+
+// import * from 'lib';
+// console.log(square(11));
+// console.log(square(4, 3));
+
+
+// export default function() {...}
+// import myFunction from 'myFunc';
+// myFunc();
--- src/StateAsync.js
+++ src/StateAsync.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react'
 export default class StateAsync extends Component {
 
 
-
 // this.setState({
 //     name:'value' 
 // },() => {
