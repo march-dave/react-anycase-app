f1739f60fcebf08aff3d7b06e67064d928505eaa
march-dave <ysangy@gmail.com>
2019-05-31 21:22:22 -0400

    descruct 2

--- src/s28_destruct1.js
+++ src/s28_destruct1.js
@@ -1,3 +1,20 @@
 let arr = [10,20,30,40];
 let [a,b,c] = arr;
 console.log(a, b, c);
+
+
+let p1 = {name:"홍길동", age:20, gender:"M"};
+let { name:n, age:a, gender } = p1;
+console.log(n,a,gender);
+
+function addContact({name, phone, email="이메일 없음", age=0}) {
+    console.log("이름 : " + name);
+    console.log("전번 : " + phone);
+    console.log("이메일 : " + email);
+    console.log("나이 : " + age);
+}
+
+addContact({
+    name : "이몽룡", 
+    phone : "010-3434-8989"
+})
