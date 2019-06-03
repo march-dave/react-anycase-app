0d15365b4918e8d4a979771677e443714e5cce16
march-dave <ysangy@gmail.com>
2019-06-03 18:53:40 -0400

    middle ware

--- src/MiddleWare.js
+++ src/MiddleWare.js
@@ -1,4 +1,15 @@
 // 미들웨어
 // const logger = store => next => action => {
 //     /* 미들웨어 내용 */
+// const logger = (store) => {
+//     return (next) => {
+//         return (action) => {
+//             if (typeof(action) !== 'undefined') {
+//                 console.log("### action 실행 : ", 
+//                     new Date().toLocaleTimeString(), JSON.stringify(action))
+//             }            
+//             return next(action);
+//         }
+//     }
+// }
 // }
