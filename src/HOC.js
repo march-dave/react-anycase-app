fec9e20d65174b56851e97560b00f5ddd4d0fb5f
march-dave <ysangy@gmail.com>
2019-06-04 19:32:51 -0400

    HOC

---
+++ src/HOC.js
@@ -0,0 +1,13 @@
+// import React, { Component } from 'react';
+
+// const withRequest = (url) => (WrappedComponent) => {
+//   return class extends Component {
+//     render() {
+//       return (
+//         <WrappedComponent {...this.props}/>
+//       )
+//     }
+//   }
+// }
+
+// export default withRequest;
