067dd5b8a25dec6b214ce7898c69ac42e8148cf0
march-dave <ysangy@gmail.com>
2019-06-05 19:37:26 -0400

    generator

---
+++ src/ReduxSaga.js
@@ -0,0 +1,17 @@
+// function *foo() {
+//     try {
+//         var x = yield 3;
+//         console.log( "x: " + x ); // may never get here!
+//     }
+//     catch (err) {
+//         console.log( "Error: " + err );
+//     }
+// }
+
+// var it = foo();
+
+// var res = it.next(); // { value:3, done:false }
+
+// // instead of resuming normally with another `next(..)` call,
+// // let's throw a wrench (an error) into the gears:
+// it.throw( "Oops!" ); // Error: Oops!
