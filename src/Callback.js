162afb04c81d56ccc43cd8671f9734ff42bddb9e
march-dave <ysangy@gmail.com>
2019-07-04 21:20:08 -0400

    call back async case

--- src/Callback.js
+++ src/Callback.js
@@ -1,3 +1,6 @@
+import { nodeInternals } from "stack-utils";
+
+콜백 사용법 1)
 let a = function() {
     console.log('A')
 }
@@ -7,3 +10,28 @@ function slowfunc(callback) {
 }
 
 slowfunc(a);
+
+콜백 사용법 2)
+node.js 에서 사용 할때
+
+data.txt = '안녕하세요 동기, 비동기' 있다고 가정 하고
+
+let fs = require('fs');
+console.log(1);
+let data = fs.readFileSync('data.txt',  {encoding: 'utf8'});
+console.log(data);
+
+console.log(2);
+fs.readFile('data.txt', {}, function(err, data) {
+    console.log(3);
+    console.log(data);
+})
+console.log(4);
+
+실행 결과 
+1 
+'안녕하세요 동기, 비동기'
+2
+4 
+3 
+'안녕하세요 동기, 비동기'
