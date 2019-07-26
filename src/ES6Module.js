1f029ca160916362bf6c38bff1036b9908e5359a
Dave Lee <ysangy@gmail.com>
2019-07-26 16:23:10 -0400

    ES6

--- src/ES6Module.js
+++ src/ES6Module.js
@@ -24,3 +24,31 @@
 // import Main from 'main';
 // con lib = {message: "This Is A Lib"};
 // export { lib as Lib };
+
+
+// Utiliites.js
+// export function cube(x) {
+//     return x * x * x;
+//   }
+// export const foo = Math.PI + Math.SQRT2;
+
+// App.js
+// import { cube, foo } from 'Utilities';
+// console.log(cube(3)); // 27
+// console.log(foo);    // 4.555806215962888
+
+// Or
+
+// import * as utilities from 'Utilities';
+// console.log(utilities.cube(3)); // 27
+// console.log(utilities.foo);    // 4.555806215962888
+
+
+// Utility.js
+// export default function cube(x) {
+//     return x * x * x;
+//   };
+
+// App.js
+// import sdfjsdljfsad from 'cube';
+// console.log(sdfjsdljfsad(3)); // 27
