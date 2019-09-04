27315df1cec6e79110e9c7caf09c68ddfda2bbe6
march-dave <ysangy@gmail.com>
2019-09-04 14:52:09 -0400

    hooks

--- src/Hook.js
+++ src/Hook.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
+// React Hooks 은 버전 16.8 이상에서 지원 한다.
+// 라이프 사이클은 componentDidMount, componentDidUnmount, componentWillUnmount 만 지원한다.
+
 const Sample = (props) => {
     const [count, setCount] = useState(0);
 
