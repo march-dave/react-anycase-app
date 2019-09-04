6272a878b0bbdb4ef794473091ddc2c3bf147183
march-dave <ysangy@gmail.com>
2019-09-04 15:00:11 -0400

    reduk hook

---
+++ src/HookRedux.js
@@ -0,0 +1,33 @@
+// const mapStateToProps = (state)=> {
+//     return {
+//         todolist : state.todolist
+//     }
+// }
+
+// const mapDispatchToProps = (dispatch)=> {
+//     return {
+//         addTodo : (todo) => dispatch(TodoActionCreator.addTodo(todo)),
+//         deleteTodo : (no) => dispatch(TodoActionCreator.deleteTodo(no)),
+//         toggleDone : (no) => dispatch(TodoActionCreator.toggleDone(no)),
+//     }
+// }
+
+// const AppContainer = connect(mapStateToProps, mapDispatchToProps)(App);
+
+//import { useDispatch, useSelector } from 'react-redux'
+// const AppContainer = () => {
+//     const dispatch = useDispatch()
+
+//     var propsObject = {
+//         todolist : useSelector(state => state.todolist),
+//         addTodo : (todo) => dispatch(TodoActionCreator.addTodo(todo)),
+//         deleteTodo : (no) => dispatch(TodoActionCreator.deleteTodo(no)),
+//         toggleDone : (no) => dispatch(TodoActionCreator.toggleDone(no)),
+//     }
+
+//     return (
+//         <App {...propsObject} />
+//     );
+// };
+
+// export default AppContainer;
