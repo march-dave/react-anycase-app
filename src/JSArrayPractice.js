75046efa14d8be3159d8ad040bb1f346cfcf2dc7
march-dave <ysangy@gmail.com>
2019-11-26 06:34:43 -0500

    Ugly Number

--- src/JSArrayPractice.js
+++ src/JSArrayPractice.js
@@ -260,3 +260,30 @@
 
 // };
 // console.log(moveZeroes([0,1,0,3,12]))
+
+
+// 263. Ugly Number
+// Write a program to check whether a given number is an ugly number.
+
+// Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+// var isUgly = function(num) {
+    
+//     while(num > 2) {
+        
+//         if (num % 2 === 0) {
+//             num /= 2;
+//         } else if (num % 3 === 0) {
+//             num /= 3;
+//         } else if (num % 5 === 0) {
+//             num /= 5;
+//         } else {
+//             return false;
+//         }
+//     }
+  
+//     return true;
+// };
