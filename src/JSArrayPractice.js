b56ff7eae31e88e9754109168e53b6759c4a4bdb
march-dave <ysangy@gmail.com>
2019-11-26 19:20:27 -0500

    Ugly Number

--- src/JSArrayPractice.js
+++ src/JSArrayPractice.js
@@ -272,20 +272,13 @@
  */
 // var isUgly = function(num) {
     
-//     while(num > 2) {
-        
-//         if (num % 2 === 0) {
-//             num /= 2;
-//         } else if (num % 3 === 0) {
-//             num /= 3;
-//         } else if (num % 5 === 0) {
-//             num /= 5;
-//         } else {
-//             return false;
-//         }
-//     }
-  
-//     return true;
+//     if (num <= 0) return false;
+//     if (num === 1) return true;
+//     while (num % 2 === 0) num /= 2;
+//     while (num % 3 === 0) num /= 3;
+//     while (num % 5 === 0) num /= 5;
+    
+//     return num === 1;
 // };
 
 
