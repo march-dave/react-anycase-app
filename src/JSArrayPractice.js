71b69293c24f83d4d39af2ee20171e40d6d8dc74
march-dave <ysangy@gmail.com>
2019-12-05 22:37:17 -0500

    3Sum

--- src/JSArrayPractice.js
+++ src/JSArrayPractice.js
@@ -527,6 +527,8 @@
             if (tar === (nums[j] + nums[k]) ) {
                 res.push(nums[i], nums[j], nums[k])
             }
+            while (j < k && nums[j] === nums[j + 1]) j++;
+            while (j < k && nums[k] === nums[k - 1]) k--;
             j++;
             k--;
         }
