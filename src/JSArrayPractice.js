7e2d2d13b5f8e20ff79dff352bc8511678f9ac67
march-dave <ysangy@gmail.com>
2019-12-17 06:53:05 -0500

    threeSumClosest

--- src/JSArrayPractice.js
+++ src/JSArrayPractice.js
@@ -612,29 +612,36 @@
 //     // target = 1
 //     // 배열중 3개의 값을 더해서 target 값이 나오는 배열을 찾기
 //     // [-1, 2, 1] 는 -1 + 2 + 1 ==> 2 이지만 target = 1 의 가장 가까운 값 2를 반환! 
-   // 어떻게 풀어야 하나. 모르겠네. 2019-12-15
+//     // 어떻게 풀어야 하나. 모르겠네. 2019-12-15
     
-    // 순서를 한번은 정렬을 하고
-    // nums.sort( (a, b) => (a - b) );
+//     // 순서를 한번은 정렬을 하고
+//     nums.sort( (a, b) => (a - b) );
     
 //     let sum = 0;
 //     let res = 0;
-//     let j = 0;
+//     let j = 0, k = 0;
+//     let diff = 9999; // 처음에 그냥 어마 어마 하게 큰 랜덤값을 넣어 둔다.
     
-//     for(let i=0; i<nums.length - 1; i++) {    
+//     for(let i=0; i<nums.length - 2; i++) {    
 //         j = i+ 1;   // 시작 다음 부터
 //         k = nums.length - 1;    // 꼬리 부터
+        
+//         while( j < k ) {
+//             // 3개를 더해서 target의 가까운 값이면 된다
+//             sum = nums[i] + nums[j] + nums[k];
 
-//         // 3개를 더해서 target의 가까운 값이면 된다
-//         sum = nums[i] + nums[j] + nums[k];
-        
-//         res = Math.abs(target - sum);
-        
-//         res  = sum;
+//             let newDiff = sum - target;
+
+//             res  = sum;
+            
+//             if(Math.abs(newDiff) < diff) res = sum;
+//             diff = Math.min(Math.abs(newDiff), diff);
+            
+//             if(newDiff >= 0) k--;
+//             else j++;
+//         }
 //     }
-    
 //     return res;
-    
 // };
 
 // 26. Remove Duplicates from Sorted Array
@@ -719,28 +726,5 @@
 //     return max;
 // };
 
-// var threeSumClosest = function(nums, target) {
-//     let diff = Infinity;
-//     let container = null;
-    
-//     nums.sort((a,b) => a - b);
-    
-//     for(let i = 0; i < nums.length - 2; i++){
-//         let j = i + 1;
-//         let k = nums.length - 1;
-        
-//         while(j < k){
-//             let sum = nums[i] + nums[j] + nums[k];
-//             let newDiff = sum - target;
-            
-//             if(Math.abs(newDiff) < diff) container = sum;
-//             diff = Math.min(Math.abs(newDiff), diff);
-            
-//             if(newDiff >= 0) k--;
-//             else j++;
-//         }
-//     }
-//     return container
-    
-// };
+
 
