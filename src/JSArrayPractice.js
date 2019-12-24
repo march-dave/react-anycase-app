b21c9c74eb1ef8012ca5a71ce1cf1aa699560373
march-dave <ysangy@gmail.com>
2019-12-24 07:31:17 -0500

    Linked List append

--- src/JSArrayPractice.js
+++ src/JSArrayPractice.js
@@ -854,21 +854,21 @@
 // }
 
 
-LinkedList.prototype.append = function(data) { 
-    var node = new Node(data); var curr; 
-    if( this._head == null ) { 
-        this._head = node; 
-    } else { 
-        curr = this._head; 
-        while( curr.next ) { 
-            curr = curr.next; 
-        } 
-        curr.next = node; 
-    } 
-    this._length ++; 
-}; 
+// LinkedList.prototype.append = function(data) { 
+//     var node = new Node(data); var curr; 
+//     if( this._head == null ) { 
+//         this._head = node; 
+//     } else { 
+//         curr = this._head; 
+//         while( curr.next ) { 
+//             curr = curr.next; 
+//         } 
+//         curr.next = node; 
+//     } 
+//     this._length ++; 
+// }; 
 
-var list = new LinkedList(); 
-list.append(15); 
-list.append(10);
+// var list = new LinkedList(); 
+// list.append(15); 
+// list.append(10);
 
