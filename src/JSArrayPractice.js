2a982166ab9ee7ed4eba0d592b84e7ac794a98e1
march-dave <ysangy@gmail.com>
2019-12-28 19:24:37 -0500

    up to date

--- src/JSArrayPractice.js
+++ src/JSArrayPractice.js
@@ -894,3 +894,17 @@
 //     } 
 //     return null; 
 // };
+
+
+// 1299. Replace Elements with Greatest Element on Right Side
+// Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+
+// After doing so, return the array.
+
+// /**
+//  * @param {number[]} arr
+//  * @return {number[]}
+//  */
+// var replaceElements = function(arr) {
+//     // 뭘 하라는 건지?
+// };
