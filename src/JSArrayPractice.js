a97927afe36e3f0ded9f0c89e61d20889e413aed
march-dave <ysangy@gmail.com>
2019-12-31 20:03:08 -0500

    mergeTwoLists

--- src/JSArrayPractice.js
+++ src/JSArrayPractice.js
@@ -944,3 +944,21 @@
 //     }
 //     return stack.length === 0;    
 // };
+
+21. Merge Two Sorted Lists
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+// var mergeTwoLists = function(l1, l2) {
+  
+// };
