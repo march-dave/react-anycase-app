54e68efb3bf6ce1c4f085a2488995a8fc5a6cf98
march-dave <ysangy@gmail.com>
2020-01-18 11:07:21 -0500

    up to date

--- src/JSArrayPractice.js
+++ src/JSArrayPractice.js
@@ -1188,8 +1188,8 @@
 // 탐색             8
 // 정렬, DP        11
 
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split(' ');
-let a = parseInt(input[0]);
-let b = parseInt(input[1]);
-console.log(a+b);
+// let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+// let a = parseInt(input[0]);
+// let b = parseInt(input[1]);
+// console.log(a+b);
