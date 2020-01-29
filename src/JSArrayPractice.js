621982545b4e54612f073d32cfdb03c5c317d3e4
march-dave <ysangy@gmail.com>
2020-01-28 21:21:26 -0500

    up to date

--- src/JSArrayPractice.js
+++ src/JSArrayPractice.js
@@ -1200,7 +1200,7 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 // var nextPermutation = function(nums) {
-//     // 1,2,3 넣었는데 1,3,2 
+//     // 1,2,3 넣었는데 1, 3, 2 
 //     // 3, 2, 1 --> 1, 2, 3
 //     // 1, 1, 5 --> 1, 5, 1
 // };
