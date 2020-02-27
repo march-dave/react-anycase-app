521984c3124bdc0edef85a3f0709ce604f0d57aa
march-dave <ysangy@gmail.com>
2020-02-26 20:29:41 -0500

    uniquePaths

--- src/JSArrayPractice.js
+++ src/JSArrayPractice.js
@@ -1349,3 +1349,20 @@
     
 //     return memo[nums.length];
 // };
+
+
+// var uniquePaths = function(m, n) {
+//     let dp=[];
+//     for (let i = 0; i <= m; i++) {
+//         dp[i] = [];
+//     }
+//     dp[0][0]=0;
+//     for(let i=1;i<=m;i++){
+//         for(let j=1;j<=n;j++){
+//         if(i===1) dp[i][j]=1;
+//         if(j===1) dp[i][j]=1;
+//         if(i!==1&&j!==1) dp[i][j]=dp[i][j-1]+dp[i-1][j];
+//         }
+//     }
+//     return dp[m][n];
+// };
