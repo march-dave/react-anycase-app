78dbc1c5f04964fd61f9b9ffb6c40f6232a38b26
march-dave <ysangy@gmail.com>
2020-02-27 20:20:55 -0500

    up to date

--- src/JSArrayPractice.js
+++ src/JSArrayPractice.js
@@ -1351,18 +1351,18 @@
 // };
 
 
-// var uniquePaths = function(m, n) {
-//     let dp=[];
-//     for (let i = 0; i <= m; i++) {
-//         dp[i] = [];
-//     }
-//     dp[0][0]=0;
-//     for(let i=1;i<=m;i++){
-//         for(let j=1;j<=n;j++){
-//         if(i===1) dp[i][j]=1;
-//         if(j===1) dp[i][j]=1;
-//         if(i!==1&&j!==1) dp[i][j]=dp[i][j-1]+dp[i-1][j];
-//         }
-//     }
-//     return dp[m][n];
-// };
+var uniquePaths = function(m, n) {
+    let dp=[];
+    for (let i = 0; i <= m; i++) {
+        dp[i] = [];
+    }
+    dp[0][0]=0;
+    for(let i=1;i<=m;i++){
+        for(let j=1;j<=n;j++){
+        if(i===1) dp[i][j]=1;
+        if(j===1) dp[i][j]=1;
+        if(i!==1&&j!==1) dp[i][j]=dp[i][j-1]+dp[i-1][j];
+        }
+    }
+    return dp[m][n];
+};
