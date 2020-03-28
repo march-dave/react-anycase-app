7fd80ac01f3603cec9bb2182450780b7e681e925
march-dave <ysangy@gmail.com>
2020-03-27 20:29:13 -0400

    up to date

--- src/Dummy.js
+++ src/Dummy.js
@@ -1404,5 +1404,13 @@
 // var isSymmetric = function(root) {
 //     // 이건 도대체 어떻게 푸는거냐?
 //     // binary tree or not check 
-    
+//      // return helper(root, root)
 // };
+
+// const helper = function(p, q) {
+//     if (p == null && q == null)
+//         return true
+//     if (p == null || q == null)
+//         return false
+//     return p.val == q.val && helper(p.left, q.right) && helper(p.right, q.left)
+// }
