e7b640f112902e410ccf8934646ecca66e9a1ff3
Dave Lee <ysangy@gmail.com>
2022-03-26 18:27:22 -0400

    push

---
+++ src/useEffectAsync.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+function App(){
+  let [count, setCount] = useState(0);
+  let [age, setAge] = useState(20);
+
+  useEffect(()=>{
+    if ( count != 0 && count < 3 ) {
+      setAge(age+1)
+    }
+   }, [count]) 
+
+  return (
+    <div>
+      <div>안녕하십니까 전 {age}</div>
+      <button onClick={()=>{
+            setCount(count+1);
+            }}>누르면한살먹기
+        </button> 
+    </div>
+  )
+
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
