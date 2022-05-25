332a977b14469f3f31c0344de816a8b25ecc8077
Dave Lee <ysangy@gmail.com>
2022-05-24 20:38:49 -0400

    push

--- src/useRef.js
+++ src/useRef.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 function App() {
   const [inputValue, setInputValue] = useState("10");
-  const count = useRef(77);
+  const count = useRef(78);
   
   useEffect(() => {
     count.current = count.current + 1;
